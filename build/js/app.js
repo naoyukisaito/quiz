b4a8d71135d71b922567832f51e5876c9ecdb551
naoyuki_saito <naoyuki_saito@waku-2.com>
2016-08-22 20:45:46 +0900

    json修正

--- build/js/app.js
+++ build/js/app.js
@@ -6,150 +6,151 @@ var app = new Vue({
 				ttl: "Q.1",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
-				]
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
+				],
+				correct: [1, 3, 4]
 			},
 			{
 				ttl: "Q.2",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.3",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.4",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.5",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.6",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "q1" },
+					{ choices: "2. 探しやすい", id: "q2" },
+					{ choices: "3. 再利用しやすい", id:"q3" },
+					{ choices: "4. 拡張しやすい", id:"q4" }
 				]
 			},
 			{
 				ttl: "Q.7",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.8",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.9",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.10",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.11",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.12",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			},
 			{
 				ttl: "Q.13",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "q1" },
+					{ choices: "2. 探しやすい", id: "q2" },
+					{ choices: "3. 再利用しやすい", id:"q3" },
+					{ choices: "4. 拡張しやすい", id:"q4" }
 				]
 			},
 			{
 				ttl: "Q.14",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "q1" },
+					{ choices: "2. 探しやすい", id: "q2" },
+					{ choices: "3. 再利用しやすい", id:"q3" },
+					{ choices: "4. 拡張しやすい", id:"q4" }
 				]
 			},
 			{
 				ttl: "Q.15",
 				question: "可読性・汎用性の高い、運用しやすいコードとは？",
 				answers: [
-					{ answer: "わかりやすい", id: "q1" },
-					{ answer: "探しやすい", id: "q2" },
-					{ answer: "再利用しやすい", id:"q3" },
-					{ answer: "拡張しやすい", id:"q4" }
+					{ choices: "1. わかりやすい", id: "1" },
+					{ choices: "2. 探しやすい", id: "2" },
+					{ choices: "3. 再利用しやすい", id:"3" },
+					{ choices: "4. 拡張しやすい", id:"4" }
 				]
 			}
 		]
